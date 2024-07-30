14794dc71433a5a405bbeb94c55e03acfcc57cc7
Giovanni <giovanni.sokenou2105@gmail.com>
2024-07-30 15:37:39 +0200

    exo md

---
+++ app.js
@@ -0,0 +1,3 @@
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var newArray = numberArray.filter(function (nbr) { return nbr % 2 === 0; });
+console.log(newArray);
---
+++ app.ts
@@ -0,0 +1,3 @@
+const  number :number[]=[1,2,3,4,5,6,7,8,9,10];
+const Arr : number[] = number.filter((nbr)=>nbr %2 ===0);
+console.log(Arr);
